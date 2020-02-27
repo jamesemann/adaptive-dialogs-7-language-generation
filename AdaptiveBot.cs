f327d5657755272307e02d84bac12ff4dfa02590
James Mann <james.mann@bcs.org>
2020-02-27 11:40:11 +0000

    [jamesemann]chore: initial commit

---
+++ AdaptiveBot.cs
@@ -0,0 +1,32 @@
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Adaptive;
+using Microsoft.Bot.Builder.Dialogs.Debugging;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace adaptive_dialogs_example
+{
+    public class AdaptiveBot : ActivityHandler
+    {
+        private readonly IStatePropertyAccessor<DialogState> dialogStateAccessor;
+        private readonly ResourceExplorer resourceExplorer;
+        private readonly DialogManager dialogManager;
+
+        public AdaptiveBot(ConversationState conversationState, ResourceExplorer resourceExplorer)
+        {
+            this.dialogStateAccessor = conversationState.CreateProperty<DialogState>("RootDialogState");
+            this.resourceExplorer = resourceExplorer;
+            var resource = this.resourceExplorer.GetResource("main.dialog");
+            this.dialogManager = new DialogManager(DeclarativeTypeLoader.Load<AdaptiveDialog>(resource, resourceExplorer, DebugSupport.SourceMap));
+        }
+
+        public async override Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            await dialogManager.OnTurnAsync(turnContext, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+#### Background
+
+This is a template you can use to create experimental declarative Adaptive Dialogs using Bot Framework.
+
+#### Using the template
+
+1. Clone the repo
+2. Using your IDE of choice build the `.csproj` or use the .net core cli to execute `dotnet run`
+3. Open the `adaptive-dialogs-example.bot` with the [Bot Framework Emulator](https://github.com/Microsoft/BotFramework-Emulator/releases)
+
+This will run the composed conversation in  `Dialogs/main.dialog`.  To add your conversation simply work in this folder.
+
+![](https://user-images.githubusercontent.com/6830648/75431245-7c859980-5944-11ea-9a82-f43528bcd6cb.png)
+
